757d51d8eaa4d07e7d9d84c29503ee242dae483a
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2026-08-28 18:33:53 +0200

    Service Updates struct & DB select.

--- internal/model/gtfs_rt.go
+++ internal/model/gtfs_rt.go
@@ -1,2 +1,15 @@
 package model
 
+import "time"
+
+type ServiceAlertsResult struct {
+	Basin      string    `db:"basin" json:"basin"`
+	Id         string    `db:"id" json:"id"`
+	Start      time.Time `db:"start" json:"start"`
+	End        time.Time `db:"end" json:"end"`
+	Route_id   string    `db:"route_id" json:"route_id"`
+	Route_type string    `db:"route_type" json:"route_type"`
+	Start_time time.Time `db:"start_time" json:"start_time"`
+	Start_date time.Time `db:"start_date" json:"start_date"`
+	Stop_id    string    `db:"stop_id" json:"stop_id"`
+}
--- internal/repository/realtime/service_alerts.go
+++ internal/repository/realtime/service_alerts.go
@@ -1,10 +1,32 @@
 package realtime
 
 import (
+	"StartRomagnaAPI/internal/model"
+	"StartRomagnaAPI/internal/repository"
 	"fmt"
 
 	"github.com/MobilityData/gtfs-realtime-bindings/golang/gtfs"
 )
+
+func GetServiceAlerts() []model.ServiceAlertsResult {
+	var results []model.ServiceAlertsResult
+	err := repository.DB_RT.Select(&results, "SELECT * FROM service_alerts")
+	if err != nil {
+		fmt.Println("GetServiceAlerts error:", err)
+	}
+
+	return results
+}
+
+func GetServiceAlertsBasin(basin string) []model.ServiceAlertsResult {
+	var results []model.ServiceAlertsResult
+	err := repository.DB_RT.Select(&results, "SELECT * FROM service_alerts WHERE basin = ?", basin)
+	if err != nil {
+		fmt.Println("GetServiceAlertsBasin error:", err)
+	}
+
+	return results
+}
 
 func SaveAlerts(feeds map[string]*gtfs.FeedMessage) {
 	for _, val := range feeds {
