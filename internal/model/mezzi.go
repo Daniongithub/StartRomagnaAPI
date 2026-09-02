f80bbb61e11736a74a2f8e28fedb1a1721932d7d
Leocraft1 <leonardotoschi07@gmail.com>
2026-09-02 18:08:37 +0200

    Modified parameter on /busesinservice

--- internal/model/mezzi.go
+++ internal/model/mezzi.go
@@ -1,9 +1,9 @@
 package model
 
 type VehicleInService struct {
-	Number    string  `db:"matricola" json:"number"`
-	PlateNum  string  `db:"targa" json:"plate_num"`
-	Model     string  `db:"modello" json:"model"`
-	Basin     string  `db:"provincia" json:"basin"`
-	PhotoPath *string `db:"photo_path" json:"photo_path"`
+	Number         string  `db:"matricola" json:"number"`
+	PlateNum       string  `db:"targa" json:"plate_num"`
+	Model          string  `db:"modello" json:"model"`
+	Basin          string  `db:"provincia" json:"basin"`
+	BusPreviewPath *string `db:"photo_path" json:"bus_preview_path"`
 }
